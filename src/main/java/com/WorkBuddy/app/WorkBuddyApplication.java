78e771156d239ea355154c5248091ebb94cf6563
Svetoslav Iliev <svetlio.iliev17@gmail.com>
2023-12-16 21:47:24 +0200

    Initial commit

---
+++ src/main/java/com/WorkBuddy/app/WorkBuddyApplication.java
@@ -0,0 +1,13 @@
+package com.WorkBuddy.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WorkBuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WorkBuddyApplication.class, args);
+	}
+
+}
